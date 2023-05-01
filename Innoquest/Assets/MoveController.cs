0ccf875cb3f561738a25a35304e94a85d0bd61e6
itsChaboy <elton8@live.com.pt>
2023-05-02 00:23:40 +0100

    wii balance board controller

---
+++ Innoquest/Assets/MoveController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+	public UDPReceiver udpreceiver; // reference to the other script
+	private float X;
+	private float moveSpeed = 10;
+
+	private void Start()
+	{
+		
+
+
+	}
+
+	private void Update()
+	{
+		float X = udpreceiver.cGravityX;
+
+
+		Debug.Log (X);
+		Debug.Log (moveSpeed);
+		if (X > 0) {
+			transform.position += new Vector3 (moveSpeed * Time.deltaTime, 0f, 0f);
+		} 
+
+		else 
+		
+		{
+			transform.position += new Vector3 (moveSpeed * (-1)* Time.deltaTime, 0f, 0f);
+		}
+		 // move the GameObject on the X axis based on the value of the variable from the other script
+	}
+}
---
+++ Innoquest/Assets/UDPReceiver.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Globalization;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class UDPReceiver : MonoBehaviour
+{
+	private UdpClient udpClient;
+	private IPEndPoint endPoint;
+	public float cGravityX;
+	public float movementSpeed = 0.5f; // adjust this to change the speed of movement
+
+	void Start()
+	{
+		udpClient = new UdpClient(1204); // Replace 1234 with the port number you want to use
+		endPoint = new IPEndPoint(IPAddress.Any, 0);
+	}
+
+	void Update()
+	{
+		if (udpClient.Available > 0)
+		{
+			byte[] data = udpClient.Receive(ref endPoint);
+			string message = System.Text.Encoding.ASCII.GetString(data);
+			Debug.Log("Received message: " + message);
+
+			string[] messageParts = message.Split(',');
+			foreach (string part in messageParts)
+			{
+				if (messageParts[2] == "[$$$]CGravity_X")
+				{
+					string stringX = messageParts[4].Trim();
+					if (stringX.EndsWith (";")) 
+					{
+						stringX = stringX.Substring (0, stringX.Length - 1);
+					}
+					cGravityX = float.Parse(stringX);
+					Debug.Log("CGravity_X: " + cGravityX);
+					break;
+				}
+			}
+				
+		}
+	}
+
+	void OnDisable()
+	{
+		udpClient.Close();
+	}
+}
